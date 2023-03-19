9f12c492a67ef135c6f621472a16b628b7318196
Tiago Pinto <jotipinto@gmail.com>
2023-03-19 17:52:23 +0000

    almost the final form

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const channels = document.querySelectorAll('.channel');
-  const chatHeader = document.querySelector('.chat-header');
-  const messageInput = document.querySelector('.message-input');
-  const chat = document.querySelector('.chat');
+$(document).ready(function() {
+  const serverIcons = $('.server-icon');
+  const chatHeader = $('.chat-header');
+  const messageInput = $('.message-input');
+  const chat = $('.chat');
+  const sidebar = $('.sidebar');
 
-  channels.forEach(channel => {
-    channel.addEventListener('click', () => {
-      // Remove 'selected' class from other channels
-      channels.forEach(ch => ch.classList.remove('selected'));
+  function updateChannels(serverIndex) {
+      // Clear the existing channels
+      $('.channel').remove();
 
-      // Add 'selected' class to the clicked channel
-      channel.classList.add('selected');
+      let newChannels;
 
-      // Update chat header with the selected channel name
-      chatHeader.textContent = channel.textContent;
-    });
-  });
-
-  messageInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      const messageText = event.target.value.trim();
-
-      if (messageText) {
-        // Create a new message element
-        const newMessage = document.createElement('div');
-        newMessage.classList.add('message');
-        newMessage.innerHTML = `<span class="username">You:</span> ${messageText}`;
-
-        // Append the new message to the chat
-        chat.appendChild(newMessage);
-
-        // Scroll the chat to the bottom
-        chat.scrollTop = chat.scrollHeight;
-
-        // Clear the input
-        event.target.value = '';
+      if (serverIndex === 0) {
+          newChannels = [
+              { name: 'general', displayName: '# general' },
+              { name: 'random', displayName: '# random' },
+              { name: 'off-topic', displayName: '# off-topic' },
+          ];
+      } else if (serverIndex === 1) {
+          newChannels = [
+              { name: 'twitter', displayName: 'Twitter' },
+              { name: 'linkedin', displayName: 'LinkedIn' },
+              { name: 'github', displayName: 'GitHub' },
+          ];
+      } else if (serverIndex === 2) {
+          newChannels = [
+              { name: 'example1', displayName: 'Example 1' },
+              { name: 'example2', displayName: 'Example 2' },
+              { name: 'example3', displayName: 'Example 3' },
+          ];
       }
-    }
+
+      newChannels.forEach(channel => {
+          const newChannel = $('<div></div>')
+              .addClass('channel')
+              .text(channel.displayName)
+              .data('channelName', channel.name)
+              .on('click', function() {
+                  $('.channel').removeClass('selected');
+                  $(this).addClass('selected');
+                  chatHeader.text($(this).text());
+              });
+          sidebar.append(newChannel);
+      });
+  }
+
+  serverIcons.on('click', function() {
+      serverIcons.removeClass('selected');
+      $(this).addClass('selected');
+
+      const serverIndex = serverIcons.index(this);
+      updateChannels(serverIndex);
   });
+
+  messageInput.on('keydown', function(event) {
+      if (event.key === 'Enter') {
+          event.preventDefault();
+          const messageText = $(this).val().trim();
+
+          if (messageText) {
+              const newMessage = $('<div></div>')
+                  .addClass('message')
+                  .html(`<span class="username">You:</span> ${messageText}`);
+              chat.append(newMessage);
+              chat.scrollTop(chat.prop('scrollHeight'));
+              $(this).val('');
+          }
+      }
+  });
+
+  // Initialize channels for the first server
+  updateChannels(0);
 });
