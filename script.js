fde8661fb1c38dcfa5ac01f274ec8fe29582ef8c
Tiago Pinto <jotipinto@gmail.com>
2023-03-22 14:39:13 +0000

    final look?

--- script.js
+++ script.js
@@ -1,74 +1,17 @@
-$(document).ready(function() {
-  const serverIcons = $('.server-icon');
-  const chatHeader = $('.chat-header');
-  const messageInput = $('.message-input');
-  const chat = $('.chat');
-  const sidebar = $('.sidebar');
+var nav = document.getElementById("navlinks");
+var navlinks = nav.getElementsByClassName("navlink");
 
-  function updateChannels(serverIndex) {
-      // Clear the existing channels
-      $('.channel').remove();
-
-      let newChannels;
-
-      if (serverIndex === 0) {
-          newChannels = [
-              { name: 'general', displayName: '# general' },
-              { name: 'random', displayName: '# random' },
-              { name: 'off-topic', displayName: '# off-topic' },
-          ];
-      } else if (serverIndex === 1) {
-          newChannels = [
-              { name: 'twitter', displayName: 'Twitter' },
-              { name: 'linkedin', displayName: 'LinkedIn' },
-              { name: 'github', displayName: 'GitHub' },
-          ];
-      } else if (serverIndex === 2) {
-          newChannels = [
-              { name: 'example1', displayName: 'Example 1' },
-              { name: 'example2', displayName: 'Example 2' },
-              { name: 'example3', displayName: 'Example 3' },
-          ];
+function setActiveLink() {
+  for (var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("active");
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" active", "");
       }
-
-      newChannels.forEach(channel => {
-          const newChannel = $('<div></div>')
-              .addClass('channel')
-              .text(channel.displayName)
-              .data('channelName', channel.name)
-              .on('click', function() {
-                  $('.channel').removeClass('selected');
-                  $(this).addClass('selected');
-                  chatHeader.text($(this).text());
-              });
-          sidebar.append(newChannel);
-      });
+      this.className += " active";
+    });
   }
+}
 
-  serverIcons.on('click', function() {
-      serverIcons.removeClass('selected');
-      $(this).addClass('selected');
+setActiveLink();
 
-      const serverIndex = serverIcons.index(this);
-      updateChannels(serverIndex);
-  });
-
-  messageInput.on('keydown', function(event) {
-      if (event.key === 'Enter') {
-          event.preventDefault();
-          const messageText = $(this).val().trim();
-
-          if (messageText) {
-              const newMessage = $('<div></div>')
-                  .addClass('message')
-                  .html(`<span class="username">You:</span> ${messageText}`);
-              chat.append(newMessage);
-              chat.scrollTop(chat.prop('scrollHeight'));
-              $(this).val('');
-          }
-      }
-  });
-
-  // Initialize channels for the first server
-  updateChannels(0);
-});
